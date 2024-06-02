e0e4483335640d1df3197c0bd378fd1fd79dfbc6
Peterfi Peter <peterfipeter120@gmail.com>
2024-06-02 14:40:20 +0300

    persistent connection with MongoDB

--- server_side/database_objects/mongo_db.py
+++ server_side/database_objects/mongo_db.py
@@ -1,17 +1,29 @@
 import pymongo
 
+# MongoDB client, no outside access is allowed.
+__client__: pymongo.MongoClient | None = None
 
-class _MongoHost:
+
+def set_mongo_host(host_str: str | None = None):
     """
-    A class to represent the MongoDB host as a string.
-    This class is not meant to be used directly.
+    Set the MongoDB host string.
     """
+    global __client__
+    if __client__ is not None:
+        raise ConnectionError("MongoDB client is already connected. Close the client first.")
+    if host_str is None:
+        host_str = "mongodb://localhost:27017"
+    __client__ = pymongo.MongoClient(host_str)
 
-    def __init__(self, host_str: str = "mongodb://localhost:27017"):
-        self.__host_str: str = host_str
 
-    def __str__(self) -> str:
-        return self.__host_str
+def close_mongo_client():
+    """
+    Close the MongoDB client.
+    """
+    global __client__
+    if __client__ is not None:
+        __client__.close()
+        __client__ = None
 
 
 def insert_one(db_name: str, collection_name: str, key_value_pair: tuple[str, str]) -> str:
@@ -19,55 +31,56 @@ def insert_one(db_name: str, collection_name: str, key_value_pair: tuple[str, st
     Insert a document into a collection without any validation.
     Returns the key of the inserted document on success.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection = db[collection_name]
-        # noinspection PyUnresolvedReferences
-        try:
-            key, value = key_value_pair
-            collection.insert_one({"_id": key, "value": value})  # insert a key-value pair into mongoDB collection
-        except pymongo.errors.DuplicateKeyError:
-            # catch the error if there are duplicate keys
-            raise ValueError(f"Duplicate key [{key}] in collection [{collection_name}]")
-        return key
+    global __client__
+    db = __client__[db_name]
+    collection = db[collection_name]
+    # noinspection PyUnresolvedReferences
+    try:
+        key, value = key_value_pair
+        collection.insert_one({"_id": key, "value": value})  # insert a key-value pair into mongoDB collection
+    except pymongo.errors.DuplicateKeyError:
+        # catch the error if there are duplicate keys
+        raise ValueError(f"Duplicate key [{key}] in collection [{collection_name}]")
+    return key
 
 
 def insert_one_int(db_name: str, collection_name: str, key_value_pair: tuple[str, int]) -> str:
     """
     Insert a document into a collection with the value part as an integer.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection = db[collection_name]
-        key, value = key_value_pair
-        collection.insert_one({"_id": key, "value": value})  # insert a key-value pair into mongoDB collection
+    global __client__
+    db = __client__[db_name]
+    collection = db[collection_name]
+    key, value = key_value_pair
+    collection.insert_one({"_id": key, "value": value})  # insert a key-value pair into mongoDB collection
+    return key
 
 
 def create_collection(db_name: str, collection_name: str):
     """
     Create new collection in a database.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        db.create_collection(collection_name)
+    global __client__
+    db = __client__[db_name]
+    db.create_collection(collection_name)
 
 
 def drop_database(db_name: str):
     """
     Deletes a database only if it exists.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        if db_name in client.list_database_names():
-            client.drop_database(db_name)
+    global __client__
+    if db_name in __client__.list_database_names():
+        __client__.drop_database(db_name)
 
 
 def drop_collection(db_name: str, collection_name: str):
     """
     Deletes a collection.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        db.drop_collection(collection_name)
+    global __client__
+    db = __client__[db_name]
+    db.drop_collection(collection_name)
 
 
 def delete(db_name: str, collection_name: str, query: dict) -> int:
@@ -75,28 +88,28 @@ def delete(db_name: str, collection_name: str, query: dict) -> int:
     Deletes documents from a collection, without any validation.
     Returns the number of deleted documents.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection = db[collection_name]
-        result = collection.delete_many(query)
-        return result.deleted_count
+    global __client__
+    db = __client__[db_name]
+    collection = db[collection_name]
+    result = collection.delete_many(query)
+    return result.deleted_count
 
 
 def get_database_names() -> list[str]:
     """
     Get a list of database names.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        return client.list_database_names()
+    global __client__
+    return __client__.list_database_names()
 
 
 def get_collection_names(db_name: str) -> list[str]:
     """
     Get a list of collection names in a database.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        return db.list_collection_names()
+    global __client__
+    db = __client__[db_name]
+    return db.list_collection_names()
 
 
 def select(db_name: str, collection_name: str, selection: dict = None) -> list[dict]:
@@ -106,28 +119,28 @@ def select(db_name: str, collection_name: str, selection: dict = None) -> list[d
     :param collection_name: name of the collection
     :param selection: query to filter the documents
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection: pymongo.collection.Collection = db[collection_name]
-        result = collection.find(selection if not None else {}, {"_id": 1, "value": 1})
-        return list(result)
+    global __client__
+    db = __client__[db_name]
+    collection: pymongo.collection.Collection = db[collection_name]
+    result = collection.find(selection if not None else {}, {"_id": 1, "value": 1})
+    return list(result)
 
 
 def increment_identity(db_name: str, table_name: str, increment_by: int):
     """
     Increment the next identity value of a table in the __next_identity collection of the given database.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection_name = "__next_identity"
-        collection: pymongo.collection.Collection = db[collection_name]
-        filter_criteria = {"_id": table_name}
-        update_operation = {"$inc": {"value": increment_by}}
-        result = collection.update_one(filter_criteria, update_operation)
-        if result.matched_count == 0:
-            raise ValueError(
-                f"Failed to increment next identity value in collection [{collection_name}] for table [{table_name}]."
-            )
+    global __client__
+    db = __client__[db_name]
+    collection_name = "__next_identity"
+    collection: pymongo.collection.Collection = db[collection_name]
+    filter_criteria = {"_id": table_name}
+    update_operation = {"$inc": {"value": increment_by}}
+    result = collection.update_one(filter_criteria, update_operation)
+    if result.matched_count == 0:
+        raise ValueError(
+            f"Failed to increment next identity value in collection [{collection_name}] for table [{table_name}]."
+        )
 
 
 def update_one(db_name: str, collection_name: str, query: dict, update: dict):
@@ -135,7 +148,7 @@ def update_one(db_name: str, collection_name: str, query: dict, update: dict):
     Update a document in a collection.
     No validation is performed.
     """
-    with pymongo.MongoClient(str(_MongoHost())) as client:
-        db = client[db_name]
-        collection = db[collection_name]
-        collection.update_one(query, update)
+    global __client__
+    db = __client__[db_name]
+    collection = db[collection_name]
+    collection.update_one(query, update)
---
+++ unittests/test_mongo_db.py
@@ -0,0 +1,71 @@
+from unittest import TestCase
+
+from server_side.database_objects import mongo_db
+
+
+class TestMongoDB(TestCase):
+    """
+    Test the MongoDB functions.
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up the test.
+        """
+        mongo_db.close_mongo_client()
+        mongo_db.set_mongo_host()
+
+    def test_insert_one(self):
+        """
+        Test the insert_one function.
+        """
+        try:
+            mongo_db.drop_database("test_db")
+            key = mongo_db.insert_one("test_db", "test_collection", ("key", "value"))
+            print(key)
+
+        finally:
+            pass
+
+    def test_insert_one_int(self):
+        """
+        Test the insert_one_int function.
+        """
+        try:
+            mongo_db.drop_database("test_db")
+            key = mongo_db.insert_one_int("test_db", "test_collection", ("key", 1))
+            print(key)
+
+        finally:
+            pass
+
+    def test_create_collection(self):
+        """
+        Test the create_collection function.
+        """
+        try:
+            mongo_db.drop_database("test_db")
+            r = mongo_db.create_collection("test_db", "test_collection")
+            print(r)
+
+        finally:
+            pass
+
+    def test_drop_collection(self):
+        """
+        Test the drop_collection function.
+        """
+        try:
+            mongo_db.drop_database("test_db")
+            mongo_db.create_collection("test_db", "test_collection1")
+            mongo_db.create_collection("test_db", "test_collection2")
+
+            r = mongo_db.drop_collection("test_db", "test_collection1")
+            print(r)
+
+        finally:
+            pass
+
+
+
+
