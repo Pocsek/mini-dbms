1aede24ff904d882894e85f32855286da7b881d2
Peterfi Peter <peterfipeter120@gmail.com>
2024-06-04 14:57:26 +0300

    test files for client side command execution

---
+++ test_files/basic.sql
@@ -0,0 +1,11 @@
+drop database if exists test;
+create database test;
+use test;
+create table test_table (
+    id int primary key identity,
+    name varchar
+);
+
+-- insert into test_table (name) values ('test'), ('test2');
+-- delete from test_table where name = 'test2';
+
